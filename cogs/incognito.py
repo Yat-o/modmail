8c6432b212923be4711b5889560e09301a11f3dc
Yato <tylergoodmen312@gmail.com>
2021-07-10 00:18:51 -0400

    Add staff related command

--- cogs/incognito.py
+++ cogs/incognito.py
@@ -11,16 +11,23 @@ class Incognito(commands.Cog):
 
     @commands.command()
     @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
-    @commands.bot_has_permissions(embed_links=True)
-    async def staff_intro(self, ctx, user: discord.User):
-        """Create the staff intro announcement in #staff-announcements"""
-        chan = self.bot.get_channel(736780095855001662)
-        await chan.send(embed=discord.Embed(
-            title="**__Staff 7 day trial__**",
-            description=f"Welcome {user.mention} thanks for taking the time to apply for staff, you have passed the application. Now you will have 1 week to prove that you are a good fit for this position, at the end of the week you will be either removed or promoted based on your performance. Good luck!",
-            color=0xff0000
-        ).set_footer(text="INCOGNITO Advertising (c) |  https://discord.gg/NP4TXsZ")
-    )
+    async def addstaff(self, ctx: commands.Context, member: discord.Member):
+        admin =  ctx.guild.get_role(695051576552718416)
+        tmod = ctx.guild.get_role(736717979609464872)
+        await member.add_roles(tmod, admin)
+        await ctx.send(f"Added T-Mod and Admin Base Role to {member}")
+        await ctx.send("Creating Staff Announcement now in #staff-announcements")
+        chan = ctx.guild.get_channel(736780095855001662)
+        await chan.send(
+            embed=discord.Embed(
+                title="**__Staff 7 day trial__**",
+                description=f"Welcome {member.mention} thanks for taking the time to apply for staff, you have passed the application. Now you will have 1 week to prove that you are a good fit for this position, at the end of the week you will be either removed or promoted based on your performance. Good luck!",
+                color=0xff0000
+            ).set_footer(text="INCOGNITO Advertising (c) |  https://discord.gg/NP4TXsZ")
+        )
+
+
+
 
 def setup(bot):
     bot.add_cog(Incognito(bot))

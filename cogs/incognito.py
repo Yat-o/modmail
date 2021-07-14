af8afe1342a191399a4cca17ae75305c3bc82b5e
Yato <tylergoodmen312@gmail.com>
2021-07-13 23:45:26 -0400

    Fix command

--- cogs/incognito.py
+++ cogs/incognito.py
@@ -13,16 +13,19 @@ class Incognito(commands.Cog):
     @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
     async def addstaff(self, ctx: commands.Context, member: discord.Member):
         """Command for adding staff members to the Incognito Advertising Staff Team"""
+        roles = []
         admin =  ctx.guild.get_role(695051576552718416)
         tmod = ctx.guild.get_role(736717979609464872)
-        await member.add_roles(tmod, admin)
+        roles.append(tmod)
+        roles.append(admin)
+        await member.add_roles(*roles)
         await ctx.send(f"Added T-Mod and Base Administrator Role to {member}")
         await ctx.send("Creating Staff Announcement now in <#736780095855001662>")
         chan = ctx.guild.get_channel(736780095855001662)
         await chan.send(
             embed=discord.Embed(
                 title="**__Staff 7 day trial__**",
-                description=f"Welcome {member.mention} thanks for taking the time to apply for staff, you have passed the application. Now you will have 1 week to prove that you are a good fit for this position, at the end of the week you will be either removed or promoted based on your performance. Good luck!",
+                description=f"Welcome {member.mention} thanks for taking the time to apply for staff, you have passed the application. Please read <#736727981535264859> for information about your job here. Now you will have 1 week to prove that you are a good fit for this position, at the end of the week you will be either removed or promoted based on your performance. Good luck!",
                 color=0xff0000
             ).set_footer(text="INCOGNITO Advertising (c) |  https://discord.gg/NP4TXsZ")
         )
@@ -31,8 +34,11 @@ class Incognito(commands.Cog):
     @checks.has_permissions(PermissionLevel.ADMINISTRATOR)
     async def removestaff(self, ctx: commands.Context, member: discord.Member, *, reason = None):
         """Command for removing staff members to the Incognito Advertising Staff Team"""
+        roles = []
         admin = ctx.guild.get_role(695051576552718416)
         tmod = ctx.guild.get_role(736717979609464872)
+        roles.append(tmod)
+        roles.append(admin)
         if reason is None:
             return await ctx.send("Please add a reason to remove this staff member.")
 
@@ -40,7 +46,7 @@ class Incognito(commands.Cog):
             return await ctx.send("This member is a not a staff member.")
 
         else:
-            await member.remove_roles(tmod, admin)
+            await member.remove_roles(*roles)
             await ctx.send(f"Removed T-Mod and Base Administrator Role from {member}")
             msg = await ctx.send(f"Attempting To DM {member} termination message.")
             try:
